689dd74ee97d32390f972f6677efa2829bacf682
vivodi <103735539+vivodi@users.noreply.github.com>
2025-02-25 14:54:39 +0800

    fix

--- flexget/__init__.py
+++ flexget/__init__.py
@@ -3,6 +3,8 @@ import sys
 from collections.abc import Sequence
 from typing import Optional
 
+from loguru import logger
+
 # __version__ import need to be first in order to avoid circular import within logger
 from ._version import __version__  # noqa: F401
 
@@ -20,7 +22,7 @@ def main(args: Optional[Sequence[str]] = None):
 
         try:
             manager = Manager(args)
-        except (OSError, ValueError) as e:
+        except (OSError, ValueError):
             options = Manager.parse_initial_options(args)
             log.start(level=options.loglevel, to_file=False)
             if _is_debug():
@@ -28,7 +30,7 @@ def main(args: Optional[Sequence[str]] = None):
 
                 traceback.print_exc()
             else:
-                print(f'Could not instantiate manager: {e}', file=sys.stderr)
+                logger.opt(exception=True).critical('Could not instantiate manager:')
             sys.exit(1)
         else:
             log.start(
@@ -52,13 +54,13 @@ def main(args: Optional[Sequence[str]] = None):
                 )
             else:
                 manager.start()
-        except (OSError, ValueError) as e:
+        except (OSError, ValueError):
             if _is_debug():
                 import traceback
 
                 traceback.print_exc()
             else:
-                print(f'Could not start manager: {e}', file=sys.stderr)
+                logger.opt(exception=True).critical('Could not start manager:')
 
             sys.exit(1)
     except KeyboardInterrupt:
@@ -67,7 +69,7 @@ def main(args: Optional[Sequence[str]] = None):
 
             traceback.print_exc()
 
-        print('Killed with keyboard interrupt.', file=sys.stderr)
+        logger.critical('Killed with keyboard interrupt.')
         sys.exit(1)
 
 
